5fc48c8640d20052865d1986670ab223d8d4ef4a
Levi Ekström <vojtechlevi1@hotmail.com>
2024-03-09 20:22:40 +0100

    pages

---
+++ src/pages/Home.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Main from "../components/Main";
+import Row from "../components/Row";
+
+import requests from "../Request";
+
+const Home = () => {
+  return (
+    <div>
+      <Main />
+      <Row rowID="1" title="Popular" fetchURL={requests.requestPopular} />
+      <Row rowID="2" title="Top Rated" fetchURL={requests.requestTopRated} />
+      <Row rowID="3" title="Up Coming" fetchURL={requests.requestUpcoming} />
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/pages/Signup.jsx
@@ -0,0 +1,78 @@
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { UserAuth } from "../context/AuthContext";
+
+const Signup = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const { user, signUp } = UserAuth();
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await signUp(email, password);
+      navigate("/");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <>
+      <div className="w-full h-screen">
+        <img
+          className="hidden sm:block absolute w-full h-full object-cover"
+          src="https://assets.nflxext.com/ffe/siteui/vlv3/93da5c27-be66-427c-8b72-5cb39d275279/f8e8e28b-fd6d-4896-bae0-f8472b3c61c3/SE-sv-20240226-popsignuptwoweeks-perspective_alpha_website_small.jpg"
+          alt=""
+          srcset=""
+        />
+        <div className="bg-black/60 fixed top-0 left-0 w-full h-screen"></div>
+        <div className="fixed w-full px-4 py-24 z-50">
+          <div className="max-w-[450px] h-[600px] mx-auto bg-black/75 text-white">
+            <div className="max-w-[320px] mx-auto py-16">
+              <h1 className="text-3xl font-bold">Sign Up</h1>
+              <form
+                onSubmit={handleSubmit}
+                className="w-full flex flex-col py-4"
+              >
+                <input
+                  onChange={(e) => setEmail(e.target.value)}
+                  className="p-3 my-2 bg-gray-700 rounded"
+                  type="email"
+                  placeholder="Email"
+                  autoComplete="email"
+                />
+                <input
+                  onChange={(e) => setPassword(e.target.value)}
+                  className="p-3 my-2 bg-gray-700 rounded"
+                  type="password"
+                  placeholder="Password"
+                  autoComplete="current-password"
+                />
+                <button className="bg-red-600 py-3 my-6 rounded font-bold">
+                  Sign Up
+                </button>
+                <div className="flex items-center justify-between text-sm text-gray-600 px-4">
+                  <p>
+                    <input className="mr-2" type="checkbox" />
+                    Remember me
+                  </p>
+                  <p>Need Help?</p>
+                </div>
+                <p className="py-4 text-center">
+                  <span className="text-gray-600 mr-2">
+                    Already subscribed to Netflix?
+                  </span>
+                  <Link to="/login">Sign In</Link>
+                </p>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Signup;
